c8367de8b96feb33ccf6c6d5bec1eb2312883a95
JesenKwan <guan.zhenxing@foxmail.com>
2021-05-18 15:24:09 +0800

    update

--- truffle-config.js
+++ truffle-config.js
@@ -1,6 +1,6 @@
 const wrapProvider = require('arb-ethers-web3-bridge').wrapProvider
 const HDWalletProvider = require('@truffle/hdwallet-provider')
-const mnemonic = 'c018d9cf21333806075a7c1f32a1bbecfe3f0eecbea3bcb52dd57f98fd2caaea'
+const mnemonic = ''
 
 module.exports = {
     networks: {
